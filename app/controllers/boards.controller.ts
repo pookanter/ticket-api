5d12151004a28a546a3bdd7c0673436f97782215
sphuakpong <cloneltorrer@gmail.com>
2024-07-22 12:18:31 +0700

    init controller

---
+++ app/controllers/boards.controller.ts
@@ -0,0 +1,11 @@
+import { Get, JsonController } from 'routing-controllers';
+import { Service } from 'typedi';
+
+@JsonController()
+@Service()
+export class BoardsController {
+  @Get('/boards')
+  async getBoard() {
+    return "Hello World!"
+  }
+}
